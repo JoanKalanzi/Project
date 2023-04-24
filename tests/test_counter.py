13ba961cebeb0bdcd9eb47769811797a5582b2c9
Joan kalanzi <72227085+JoanKalanzi@users.noreply.github.com>
2023-04-24 15:45:56 +0100

    Second cahllenge

---
+++ tests/test_counter.py
@@ -0,0 +1,8 @@
+from lib.counter import *
+
+def test_counter_return_count():
+    counting = Counter()
+    counting.add(5)
+    result = counting.report()
+    
+    assert result == "Counted to 5 so far."
